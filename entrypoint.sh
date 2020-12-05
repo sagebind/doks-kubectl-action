09a80d7d1e1dcba3ee548747cceb20284cb4d27c
Stephen M. Coakley <me@stephencoakley.com>
2020-12-05 11:33:18 -0600

    Do not require v-prefix

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,12 @@ cd ${GITHUB_WORKSPACE}
 
 # Select latest stable kubectl version if none given
 if [ -z "${INPUT_KUBECTL_VERSION-}" ]; then
-    INPUT_KUBECTL_VERSION=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
+    INPUT_KUBECTL_VERSION=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt | tr -d v)
 fi
 
 # Set up kubectl
-curl -L -o ${KUBECTL_BIN} "https://storage.googleapis.com/kubernetes-release/release/${INPUT_KUBECTL_VERSION}/bin/linux/amd64/kubectl"
+echo "Downloading kubectl version ${INPUT_KUBECTL_VERSION}"
+curl -L -o ${KUBECTL_BIN} "https://storage.googleapis.com/kubernetes-release/release/v${INPUT_KUBECTL_VERSION}/bin/linux/amd64/kubectl"
 chmod +x ${KUBECTL_BIN}
 
 # Get kubeconfig
