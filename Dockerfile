52053f6c35a9a4d725b079ae601dbca13e90f607
Stephen M. Coakley <me@stephencoakley.com>
2020-12-05 11:27:43 -0600

    Add kubectl_version input

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,5 @@
 FROM digitalocean/doctl:1-latest
 
-ARG KUBECTL_VERSION=1.15.0
-
-ADD https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/linux/amd64/kubectl /usr/local/bin/kubectl
-RUN chmod +x /usr/local/bin/kubectl
 COPY entrypoint.sh /
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 set -eu
 
+KUBECTL_BIN=/usr/local/bin/kubectl
+
 cd ${GITHUB_WORKSPACE}
 
-mkdir -p ~/.kube
-/app/doctl --access-token ${INPUT_ACCESS_TOKEN} kubernetes cluster kubeconfig show ${INPUT_CLUSTER} > ~/.kube/config
-kubectl $*
+# Select latest stable kubectl version if none given
+if [ -z "${INPUT_KUBECTL_VERSION-}" ]; then
+    INPUT_KUBECTL_VERSION=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
+fi
+
+# Set up kubectl
+curl -L -o ${KUBECTL_BIN} "https://storage.googleapis.com/kubernetes-release/release/${INPUT_KUBECTL_VERSION}/bin/linux/amd64/kubectl"
+chmod +x ${KUBECTL_BIN}
+
+# Get kubeconfig
+/app/doctl --access-token ${INPUT_ACCESS_TOKEN} kubernetes cluster kubeconfig save --expiry-seconds 600 ${INPUT_CLUSTER}
+
+# Run kubectl command
+exec kubectl $*
